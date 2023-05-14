9c3d65f6abcacb1798e1f96bef5fe487bc19c552
Michael Rommel <rommel@md2v3xkc.ad005.onehc.net>
2023-05-14 03:05:37 +0200

    fix: make castles selectable

--- homesick.sh
+++ homesick.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
 export GOPATH=${HOME}/software/go
-sudo apt-get -y install golang
-go get golang.org/dl/go1.20.4
-go1.20.4 download
-go1.20.4 install github.com/charmbracelet/gum@latest
 export PATH=${HOME}/software/go/bin:${PATH}
 
-available_castles=("castle-core" "castle-tmux" "castle-coding" "castle-neovim")
+if ! gum -v; then
+	if ! go1.20.4 version; then
+		if ! go version; then
+			sudo apt-get -y install golang
+		fi
+		go get golang.org/dl/go1.20.4
+		go1.20.4 download
+	fi
+	go1.20.4 install github.com/charmbracelet/gum@latest
+fi
 
-castles=$(echo "${available_castles[@]}" | gum choose --height 10 --no-limit)
+available_castles=(castle-core castle-tmux castle-coding castle-neovim)
+
+selection=$(for c in "${available_castles[@]}"; do echo "$c"; done | gum choose --height 10 --no-limit)
+mapfile -t castles < <(echo "${selection[@]}")
 
 if [[ ${#castles[@]} -eq 0 ]]; then
 	echo "No castles to install. Aborting."
 	exit 0
 fi
 
-if [[ ! -f $HOME/.homesick/repos/homesick/homesick.sh ]]; then
-	git clone https://github.com/michaelrommel/homesick.git "$HOME/.homesick/repos/homesick"
+if [[ ! -f ${HOME}/.homesick/repos/homesick/homesick.sh ]]; then
+	git clone https://github.com/michaelrommel/homesick.git "${HOME}/.homesick/repos/homesick"
 fi
 
-source "$HOME/.homesick/repos/homesick/homesick.sh"
+source "${HOME}/.homesick/repos/homesick/homesick.sh"
 
 for castle in "${castles[@]}"; do
 	homesick -f clone "$castle"
