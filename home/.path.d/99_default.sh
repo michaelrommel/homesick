49a36f2179283c5dbe9efb7d514289fefe0b45dc
Michael Rommel <rommel@layer-7.net>
2023-05-22 22:20:29 +0200

    fix: add docker path here

--- home/.path.d/99_default.sh
+++ home/.path.d/99_default.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
 
+# list of paths that should be added
+paths=("${HOME}/.local/bin" "${HOME}/.docker/bin" "/usr/local/bin" "/usr/local/opt/avr-gcc@8/bin" "/usr/local/opt/arm-gcc-bin@8/bin")
+for p in "${paths[@]}"; do
+	if [[ -d "${p}" && ! ":${PATH}:" == *:${p}:* ]]; then
+		# path has not yet been added
+		export PATH="${p}:${PATH}"
+	fi
+done
+
 # home folder bin dir
-if [[ -d "${HOME}/bin" && ! ":${PATH}:" == *:${HOME}/bin:* ]]; then
-	# path has not yet been added
-	export PATH="${HOME}/bin:${HOME}/.local/bin:/usr/local/bin:/usr/local/opt/avr-gcc@8/bin:/usr/local/opt/arm-gcc-bin@8/bin:${PATH}"
+if [[ -d "${HOME}/bin" ]]; then
+	# path needs always to be added to the beginning
+	export PATH="${HOME}/bin:${PATH}"
 fi
