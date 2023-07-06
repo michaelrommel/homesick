53785ab631bb0ba3b3f4b0d3cee70bfe3b497311
Michael Rommel <rommel@layer-7.net>
2023-07-07 01:36:28 +0200

    fix: separate path to installed go apps

--- home/.path.d/40_go.sh
+++ home/.path.d/40_go.sh
@@ -4,9 +4,13 @@
 GOPATH=$(readlink -f "${HOME}/software")/go
 export GOPATH
 
+if [[ ! ":${PATH}:" == *:${GOPATH}/bin:* ]]; then
+	export PATH="${GOPATH}/bin:${PATH}"
+fi
+
 # look for go version manager
 if [[ -d "${HOME}/.gobrew/bin" && ! ":${PATH}:" == *:${HOME}/.gobrew/bin:* ]]; then
 	# path has not yet been added
-	export PATH="${GOPATH}/bin:${HOME}/.gobrew/current/bin:${HOME}/.gobrew/bin:${PATH}"
+	export PATH="${HOME}/.gobrew/current/bin:${HOME}/.gobrew/bin:${PATH}"
 	export GOROOT="${HOME}/.gobrew/current/go"
 fi
