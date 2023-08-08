4f94c58c0c4f875996a45d52f6d4a8d3d1c7c79d
Michael Rommel <rommel@layer-7.net>
2023-08-08 13:14:02 +0200

    fix: offer fallback rtx download

--- homesick.sh
+++ homesick.sh
@@ -38,7 +38,15 @@ install_rtx() {
 	else
 		arch="$(get_arch)"
 		mkdir -p "${HOME}/bin"
-		curl -sL "https://rtx.pub/rtx-latest-${os}-${arch}" >"${HOME}/bin/rtx"
+		if ! curl -sL "https://rtx.pub/rtx-latest-${os}-${arch}" >"${HOME}/bin/rtx"; then
+			echo "First download failed."
+			latest=$(curl -s https://api.github.com/repos/jdxcode/rtx/tags | jq -r ".[0].name")
+			echo "Latest release seems to be: ${latest}"
+			if ! curl -sL "https://github.com/jdxcode/rtx/releases/download/${latest}/rtx-${latest}-${os}-${arch}" >"${HOME}/bin/rtx"; then
+				echo "Second download failed, too. Aborting."
+				exit 1
+			fi
+		fi
 		chmod 755 "${HOME}/bin/rtx"
 		RTX="${HOME}/bin/rtx"
 	fi
