e708bf515884e7244138f66ffc350223873df8f2
Michael Rommel <rommel@layer-7.net>
2024-02-08 15:44:52 +0100

    fix: change path scripts to mise

---
+++ home/.path.d/50_mise.bash
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+if [[ -x "${HOME}/bin/mise" ]]; then
+	MISE=${HOME}/bin/mise
+elif [[ -x "/opt/homebrew/bin/mise" ]]; then
+	MISE=/opt/homebrew/bin/mise
+elif [[ -x "/usr/local/bin/mise" ]]; then
+	MISE=/usr/local/bin/mise
+elif [[ -x "/usr/bin/mise" ]]; then
+	MISE=/usr/bin/mise
+else
+	unset MISE
+fi
+
+# look for version manager
+if [[ -n "${MISE}" && ! ":${PATH}:" == *:${HOME}/.local/share/mise/shims:* ]]; then
+	# path has not yet been added
+	export PATH="${HOME}/.local/share/mise/shims:${PATH}"
+	eval "$(${MISE} activate bash)"
+fi
